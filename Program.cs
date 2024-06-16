ae3c043f88517d0628c6ae348e7140b48f4ef74a
rijanpaudel <125293260+rijanpaudel@users.noreply.github.com>
2024-06-16 15:12:52 +0545

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the number :");
+        int num = Convert.ToInt32(Console.ReadLine());
+        var even = num % 2;
+        if(even == 0)
+        {
+            Console.WriteLine("The number is even");
+        }
+        else
+        {
+            Console.WriteLine("The number is odd");
+        }
+        Console.ReadLine();
+    }
+}
