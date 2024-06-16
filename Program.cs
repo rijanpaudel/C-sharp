91538124ee70055bf96d0df3d1f968316a8fa9eb
rijanpaudel <125293260+rijanpaudel@users.noreply.github.com>
2024-06-16 14:46:55 +0545

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter your name: ");
+        var name = Console.ReadLine();
+        Console.Write("Enter your phone number: ");
+        var phoneNumber = Console.ReadLine();
+        Console.Write("Enter your age: ");
+        var age = Console.ReadLine(); ;
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Phone Numner: " + phoneNumber);
+        Console.WriteLine("Age: " + age);
+        Console.ReadLine();
+    }
+}
