6ae739d9a99697074dfeb26c9ee150709ce08311
rijanpaudel <125293260+rijanpaudel@users.noreply.github.com>
2024-06-26 13:58:32 +0545

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the first number: ");
+        String firstNum = Console.ReadLine();
+        int realFirstNum  = Convert.ToInt32(firstNum);
+        Console.WriteLine("Enter the second number: ");
+        String secondNum = Console.ReadLine();
+        int realSecondNum = Convert.ToInt32(secondNum);
+        int sum = realFirstNum + realSecondNum;
+        Console.WriteLine($"The sum is {sum}");
+        Console.ReadKey();
+
+    }
+}
