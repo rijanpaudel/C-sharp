26dc4d370cedb8a6a6acde924b94d9cf141ba2b0
rijanpaudel <125293260+rijanpaudel@users.noreply.github.com>
2024-06-26 14:38:14 +0545

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+class BasicCalculator
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.Write("Enter the first number: ");
+            String firstNum = Console.ReadLine();
+            int realFirstNum = Convert.ToInt32(firstNum);
+            Console.Write("Enter the second number: ");
+            String secondNum = Console.ReadLine();
+            int realSecondNum = Convert.ToInt32(secondNum);
+            Console.Write("Which Operation you want to do? (+ or - or * or / or %) ");
+            String operation = Console.ReadLine();
+            switch (operation)
+            {
+                case "+":
+                    Console.WriteLine(realFirstNum + realSecondNum);
+                    break;
+
+                case "-":
+                    Console.WriteLine(realFirstNum - realSecondNum);
+                    break;
+
+                case "*":
+                    Console.WriteLine(realFirstNum * realSecondNum);
+                    break;
+
+                case "/":
+                    Console.WriteLine(realFirstNum / realSecondNum);
+                    break;
+
+                case "%":
+                    Console.WriteLine(realFirstNum % realSecondNum);
+                    break;
+
+                default:
+                    Console.WriteLine("Wrong Input");
+                    break;
+            }
+
+            Console.WriteLine("Do you want to do again? (Y/N) ");
+            String yesOrNo = Console.ReadLine().ToUpper();
+            if (yesOrNo != "Y")
+            {
+                break;
+            }
+        }
+
+            Console.ReadKey();
+
+        }
+    }
